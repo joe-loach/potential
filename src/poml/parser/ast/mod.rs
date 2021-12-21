504605b963c542db4cb96792166f81100d7463d5
Joe Loach <joe.gloach@gmail.com>
2021-12-21 20:45:10 +0000

    add more helper methods to ast structs

--- src/poml/parser/ast/mod.rs
+++ src/poml/parser/ast/mod.rs
@@ -1,6 +1,8 @@
 mod builder;
+mod validation;
 
 pub use builder::*;
+pub use validation::validate;
 
 use crate::poml::{SyntaxKind, SyntaxNode};
 
@@ -41,7 +43,6 @@ node!(Name, SyntaxKind::Name);
 node!(ParamList, SyntaxKind::ParamList);
 node!(Param, SyntaxKind::Param);
 
-
 impl Root {
     pub fn stmts(&self) -> impl Iterator<Item = Stmt> {
         self.syntax().children().filter_map(Stmt::cast)
@@ -80,6 +81,11 @@ impl Stmt {
             .or_else(|| Object::cast(self.0.clone()).map(StmtKind::Object))
             .unwrap()
     }
+
+    pub fn params(&self) -> Option<impl Iterator<Item = Param>> {
+        let list = self.syntax().children().find_map(ParamList::cast)?;
+        Some(list.params())
+    }
 }
 
 impl Shape {
@@ -90,14 +96,69 @@ impl Shape {
     pub fn name(&self) -> Option<Name> {
         self.syntax().children().find_map(Name::cast)
     }
-
-    pub fn param_list(&self) -> Option<ParamList> {
-        self.syntax().children().find_map(ParamList::cast)
-    }
 }
 
 impl ParamList {
-    pub fn params(&self) -> impl Iterator<Item = Param> {
+    fn params(&self) -> impl Iterator<Item = Param> {
         self.syntax().children().filter_map(Param::cast)
+    }
+}
+
+#[derive(Debug)]
+pub enum ParamKind {
+    Name(Name),
+    Value(Value),
+}
+
+impl Param {
+    pub fn kind(&self) -> ParamKind {
+        let child = self.syntax().children().next().unwrap();
+        ParamKind::cast(child).unwrap()
+    }
+}
+
+impl AstNode for ParamKind {
+    fn cast(node: SyntaxNode) -> Option<Self>
+    where
+        Self: Sized,
+    {
+        Name::cast(node.clone())
+            .map(ParamKind::Name)
+            .or_else(|| Value::cast(node.clone()).map(ParamKind::Value))
+    }
+
+    fn syntax(&self) -> &SyntaxNode {
+        match self {
+            ParamKind::Name(it) => it.syntax(),
+            ParamKind::Value(it) => it.syntax(),
+        }
+    }
+}
+
+impl Name {
+    pub fn text(&self) -> Option<String> {
+        self.syntax().green().children().find_map(|it| {
+            it.as_token().and_then(|t| {
+                if t.kind() == SyntaxKind::Ident.into() {
+                    Some(t.text().to_string())
+                } else {
+                    None
+                }
+            })
+        })
+    }
+}
+
+impl Value {
+    pub fn value(&self) -> Option<f32> {
+        self.syntax().green().children().find_map(|it| {
+            it.as_token().and_then(|t| {
+                if t.kind() == SyntaxKind::Literal.into() {
+                    t.text().parse::<f32>().ok()
+                } else {
+                    None
+                }
+            })
+        })
     }
 }
